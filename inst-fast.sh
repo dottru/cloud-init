c27c8cff5d776ec1826aeb75954c29ad95902b91
root <root@ip-10-216-38-177.us-west-2.compute.internal>
2013-10-10 01:23:03 +0000

    initial

---
+++ inst-fast.sh
@@ -0,0 +1,6 @@
+sudo add-apt-repository ppa:apt-fast/stable
+sudo apt-get update
+sudo apt-get install apt-fast
+sudo apt-fast update
+sudo apt-fast dist-upgrade
+apt-fast install $(cat packages)
---
+++ userdata.sh
@@ -0,0 +1,4 @@
+cd /var
+git clone https://github.com/dottru/cloud-init
+cd cloud-init
+bash inst-fast.sh
